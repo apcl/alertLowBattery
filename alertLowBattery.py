532e9cb43e5292c74d55ef83bdc75fb1922f3345
Alejandro Cofré <alejandropcl@gmail.com>
2016-07-01 15:24:41 -0300

    first input

---
+++ alertLowBattery.py
@@ -0,0 +1,21 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+import time
+import sl4a
+
+droid = sl4a.Android()
+time.sleep(1)
+alerta = 20
+while True:
+      droid.batteryStartMonitoring()
+      status = droid.batteryGetLevel()
+      level = int (status [1])
+      if (level <= alerta):
+          droid.ttsSpeak ('bateria baja')
+      droid.batteryStopMonitoring()
+      time.sleep (5)
+
+
+
